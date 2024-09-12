90216e60a4804bfd18207c36b71bd78e832be0bf
JoseFloresSerpa <u201711534@upc.edu.pe>
2024-09-11 22:42:46 -0500

    Home y Profile

--- app/src/main/java/com/example/roommates/HomeActivity.kt
+++ app/src/main/java/com/example/roommates/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.roommates
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,12 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+    }
+
+    fun IrLogin(view: View){
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
     }
 }
