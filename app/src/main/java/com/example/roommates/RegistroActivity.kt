a16750f0692cd3bc8d909ab6acec8a145d1edc9f
JoseFloresSerpa <u201711534@upc.edu.pe>
2024-10-03 22:28:36 -0500

    Funciones de registro y login de usuario completadas

--- app/src/main/java/com/example/roommates/RegistroActivity.kt
+++ app/src/main/java/com/example/roommates/RegistroActivity.kt
@@ -7,52 +7,74 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.roommates.DAO.UsuarioDAO
 
 
 class RegistroActivity : AppCompatActivity() {
-    private lateinit var correoEditText: EditText
-    private lateinit var nombresEditText: EditText
-    private lateinit var apellidosEditText: EditText
-    private lateinit var telefonoEditText: EditText
-    private lateinit var contraseñaEditText: EditText
-    private lateinit var confirmarContraseñaEditText: EditText
-    private lateinit var registrarButton: Button
-    private lateinit var cancelarButton: Button
+
+    private lateinit var usuarioDAO: UsuarioDAO
+    private lateinit var correoElectronicoI: EditText
+    private lateinit var nombreI: EditText
+    private lateinit var apellidoI: EditText
+    private lateinit var telefonoI: EditText
+    private lateinit var contrasenaI: EditText
+    private lateinit var contrasenaCI: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
 
-        correoEditText = findViewById(R.id.editTextText8)
-        nombresEditText = findViewById(R.id.editTextText9)
-        apellidosEditText = findViewById(R.id.editTextText10)
-        telefonoEditText = findViewById(R.id.editTextText11)
-        contraseñaEditText = findViewById(R.id.editTextText12)
-        confirmarContraseñaEditText = findViewById(R.id.editTextText13)
-        registrarButton = findViewById(R.id.button4)
-        cancelarButton = findViewById(R.id.button5)
+        usuarioDAO = UsuarioDAO(this)
 
-        registrarButton.setOnClickListener {
-            val correo = correoEditText.text.toString()
-            val nombres = nombresEditText.text.toString()
-            val apellidos = apellidosEditText.text.toString()
-            val telefono = telefonoEditText.text.toString()
-            val contraseña = contraseñaEditText.text.toString()
-            val confirmarContraseña = confirmarContraseñaEditText.text.toString()
+        correoElectronicoI = findViewById(R.id.correoElectronico)
+        nombreI = findViewById(R.id.nombres)
+        apellidoI = findViewById(R.id.apellidos)
+        telefonoI = findViewById(R.id.telefonoUsuario)
+        contrasenaI = findViewById(R.id.contrasenaUser)
+        contrasenaCI = findViewById(R.id.contrasenaConfirmar)
 
-            if (correo.isEmpty() || nombres.isEmpty() || apellidos.isEmpty() || telefono.isEmpty() || contraseña.isEmpty() || confirmarContraseña.isEmpty()) {
-                Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show()
-            } else if (contraseña != confirmarContraseña) {
-                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
-
-            }
+        val registroBtn = findViewById<Button>(R.id.btnRegistrar)
+        registroBtn.setOnClickListener {
+            registrarUsuario()
         }
 
-        fun IrLogin(view: View){
-            val intent = Intent(this,LoginActivity::class.java)
-            startActivity(intent)
+
+    }
+
+    private fun registrarUsuario() {
+
+        val correoElectronico = correoElectronicoI.text.toString()
+        val nombre = nombreI.text.toString()
+        val apellido = apellidoI.text.toString()
+        val telefono = telefonoI.text.toString()
+        val contrasena = contrasenaI.text.toString()
+        val contrasenaConfirmar = contrasenaCI.text.toString()
+
+        if (correoElectronico.isBlank()|| nombre.isBlank() || apellido.isBlank() || telefono.isBlank() || contrasena.isBlank()) {
+            Toast.makeText(this, "Faltan ingresar campos", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (contrasena != contrasenaConfirmar ) {
+            Toast.makeText(this, "Contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        val id = usuarioDAO.insertar(correoElectronico,nombre, apellido, telefono, contrasena, "")
+
+        if (id > 0) {
+            Toast.makeText(this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            Toast.makeText(this, "Error al registrar el usuario", Toast.LENGTH_SHORT).show()
         }
     }
+
+
+    fun IrLogin(view: View){
+        val intent = Intent(this,LoginActivity::class.java)
+        startActivity(intent)
+    }
+
 }
