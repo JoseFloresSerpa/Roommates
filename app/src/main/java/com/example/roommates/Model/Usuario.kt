82da5decac49368f5f058484488921584375df87
JoseFloresSerpa <u201711534@upc.edu.pe>
2024-10-03 20:44:11 -0500

    Agregar usuario

---
+++ app/src/main/java/com/example/roommates/Model/Usuario.kt
@@ -0,0 +1,11 @@
+package com.example.roommates.Model
+
+class Usuario {
+    var id:Int=0
+    lateinit var nombre:String
+    lateinit var apellido:String
+    lateinit var telefono:String
+    lateinit var contrasena:String
+    lateinit var imagen:String
+
+}
